092ef30c03152ca87cc7f6a3f59e5bec2efdf4b4
annastasyshyn <anna.stasyshynn@gmail.com>
2024-02-23 08:25:09 +0200

    small changes

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+BUILDINGS=["ЦШ","ХС","БФК"]
+CURRENT_BUILDING="ЦШ"
+CURRENT_FLOOR=1
+FLOORS={"ЦШ":5,"БФК":5,"ХС":2}
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_index():
-    with open("templates/index.html", "r") as file:
-        html_content = file.read()
-    return html_content
+async def read_index(request: Request, id=None, buildings=BUILDINGS, floors=FLOORS, building=CURRENT_BUILDING, floor=CURRENT_FLOOR):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "id": id, "buildings": buildings, \
+"floors": floors, "building": building, "floor": floor}
+    )
 
 if __name__ == "__main__":
     import uvicorn
