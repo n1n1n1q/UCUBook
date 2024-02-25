526f2fb7b7776b0690b3a0f30a0a84ff9cc69deb
Oleh Basystyi <basystyo@gmail.com>
2024-02-25 21:13:20 +0200

    Optimized db module

--- src/db/db.py
+++ src/db/db.py
@@ -1,73 +1,85 @@
+"""
+DB module
+"""
 import os
 from google.cloud import bigquery
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "src/db/magnetic-nimbus-414610-2bcd9c115d01.json"
 client = bigquery.Client()
-project_id = client.project
-dataset_id = 'ucubook'
-dataset=bigquery.Dataset(f"{client.project}.{dataset_id}")
+proj_id = client.project
+data_id = 'ucubook'
+dataset=bigquery.Dataset(f"{client.project}.{data_id}")
 dataset.location="EU"
 
-def get_building(building,project_id=project_id,dataset_id=dataset_id,table_id="building"):
+def get_data(datatype, name):
+    """
+    Get data from the database
+    """
+    _name=""
+    match datatype:
+        case "users":
+            table_id="users"
+            _name="login"
+        case "building":
+            table_id="building"
+            _name="name"
+        case "rooms":
+            table_id="rooms"
+            _name="name"
+        case "requests":
+            table_id="requests"
+            _name="room_name"
+        case _:
+            raise ValueError(f"Invalid data type {datatype}")
     sql_query = f"""
     SELECT *
-    FROM `{project_id}.{dataset_id}.{table_id}`
-    WHERE name = '{building}'
+    FROM `{proj_id}.{data_id}.{table_id}`
+    WHERE {_name} = '{name}'
 """
-    query_job = client.query(sql_query)
+    query_job=client.query(sql_query)
     row_data=[dict(row.items()) for row in query_job]
     if len(row_data)==0:
-        raise ValueError(f"Building {building} not found")
+        raise ValueError(f"{datatype} {name} not found")
     if len(row_data)>1:
-        raise ValueError(f"Multiple buildings with name {building} found")
+        raise ValueError(f"Multiple {datatype} with name {name} found")
     return row_data[0]
 
-def get_room(room,project_id=project_id,dataset_id=dataset_id,table_id="rooms"):
-    sql_query = f"""
-    SELECT *
-    FROM `{project_id}.{dataset_id}.{table_id}`
-    WHERE name = '{room}'
-"""
-    query_job = client.query(sql_query)
-    row_data=[dict(row.items()) for row in query_job]
-    if len(row_data)==0:
-        raise ValueError(f"Room {room} not found")
-    if len(row_data)>1:
-        raise ValueError(f"Multiple rooms with name {room} found")
-    return row_data[0]
-
-def get_user(user,project_id=project_id,dataset_id=dataset_id,table_id="users"):
-    sql_query = f"""
-    SELECT *
-    FROM `{project_id}.{dataset_id}.{table_id}`
-    WHERE name = '{user}'
-"""
-    query_job = client.query(sql_query)
-    row_data=[dict(row.items()) for row in query_job]
-    if len(row_data)==0:
-        raise ValueError(f"User {user} not found")
-    if len(row_data)>1:
-        raise ValueError(f"Multiple users with name {user} found")
-    return row_data[0]
-    
-def add_user(table,data,dataset_id=dataset_id):
-    table_id=dataset_id+"."+table
+def add_data(datatype, data):
+    """
+    Add data to the database
+    """
+    if not isinstance(data, dict):
+        raise ValueError("Invalid input data")
+    match datatype:
+        case "users":
+            table_id="users"
+            if set(data.keys())!=set(["login","password","can_rent","group"]):
+                raise ValueError("Invalid input data")
+        case "building":
+            table_id="building"
+            if set(data.keys())!=set(["name","floors"]):
+                raise ValueError("Invalid input data")
+        case "rooms":
+            table_id="rooms"
+            if set(data.keys())!=set(["name","capacity","is_free"]):
+                raise ValueError("Invalid input data")
+        case "requests":
+            table_id="requests"
+            if set(data.keys())!=set(["room_name","renter","busy_from","busy_to","day"]):
+                raise ValueError("Invalid input data")
+        case _:
+            raise ValueError(f"Invalid data type {datatype}")
+    table_id=data_id+"."+table_id
     errors = client.insert_rows_json(table_id, data)
-    if errors == []:
+    if not errors:
         print("New rows have been added.")
     else:
-        print("Encountered errors while inserting rows: {}".format(errors))
-    
+        print(f"Encountered errors while inserting rows: {errors}")
 
-def data_init(table,data,dataset_id=dataset_id):
-    table_id=dataset_id+"."+table
-    errors = client.insert_rows_json(table_id, data)
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
-data=[
-    {"name":"ЦШ", "floors":5},
-    {"name":"БФК", "floors":5},
-    {"name":"ХС", "floors": 5}
-]
-print(get_building("ЦШ"))
+if __name__=='__main__':
+    data=[
+        {"name":"ЦШ", "floors":5},
+        {"name":"БФК", "floors":5},
+        {"name":"ХС", "floors": 5}
+    ]
+
+    print(get_data("building","ЦШ"))
--- src/main.py
+++ src/main.py
@@ -2,23 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from .db import db
 app = FastAPI()
-
-BUILDINGS=["ЦШ","ХС","БФК"]
-CURRENT_BUILDING="ЦШ"
-CURRENT_FLOOR=1
-FLOORS={"ЦШ":5,"БФК":5,"ХС":2}
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_index(request: Request, id=None, buildings=BUILDINGS, floors=FLOORS, building=CURRENT_BUILDING, floor=CURRENT_FLOOR):
+async def read_index(request: Request, id=None):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request, "id": id, "buildings": buildings, \
-"floors": floors, "building": building, "floor": floor}
+        {"request": request, "id": id}
     )
 
 if __name__ == "__main__":
