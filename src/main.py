b69dba0946b023f363f6f5661a810a407734aa74
orest <orest.chupa@gmail.com>
2024-03-02 11:53:22 +0300

    Add login action

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status, Form
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 import db.db as db
+from models import *
 database = db.DBOperations()
 app = FastAPI()
 
@@ -36,7 +40,23 @@ async def read_requests(request: Request):
         "user_requests.html",
         {"request": request, "id": id}
     )
-
+@app.post("/login")
+def add(request: Request, name: str = Form(...), password: str = Form(...)):
+    name = name
+    password = password
+    try:
+        user_data = database.get_data('users', name)
+        if user_data['password'] == password:
+            current_user = user_data
+        else:
+            current_user = None
+    except ValueError:
+        current_user = None
+    if current_user:
+        url = app.url_path_for('read_index')
+        return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
+    url = app.url_path_for("read_login")
+    return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
