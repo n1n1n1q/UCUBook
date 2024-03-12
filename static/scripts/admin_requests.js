331379fc728c25f06ab6563f3a18564290748c6c
Oleh Basystyi <basystyo@gmail.com>
2024-03-12 02:20:18 +0200

    added pagination system

--- static/scripts/admin_requests.js
+++ static/scripts/admin_requests.js
@@ -1,167 +1,203 @@
 const container = document.getElementById('pendingRequestsContainer');
 const loadingText = document.createElement('p');
 loadingText.textContent = 'Завантаження даних...';
-loadingText.style.fontSize="24px";
-loadingText.style.marginLeft="10px";
-loadingText.style.marginBottom="40vw";
+loadingText.style.fontSize = "24px";
+loadingText.style.marginLeft = "10px";
+loadingText.style.marginBottom = "40vw";
 container.appendChild(loadingText);
-fetch('/get_pending_requests')
-.then(response => response.json())
-.then(pendingRequests => {
 
+function createPaginationArrows(numPages, currentPage, container, requests, requestType) {
+  const pagesDiv = document.createElement('div');
+  pagesDiv.classList.add('pages');
 
-    if (pendingRequests.length === 0) {
-        loadingText.textContent = 'Наразі немає активних запитів';
+  const prevArrow = document.createElement('span');
+  prevArrow.classList.add('arrow', 'prev');
+  prevArrow.innerHTML = '←';
+  prevArrow.addEventListener('click', () => showPage(currentPage - 1, requests, container, requestType));
+  if (currentPage === 1) {
+    prevArrow.classList.add('disabled');
+  }
+  pagesDiv.appendChild(prevArrow);
+
+  const pageInfo = document.createElement('span');
+  pageInfo.classList.add('page-info');
+  pageInfo.textContent = `${currentPage} / ${numPages}`;
+  pagesDiv.appendChild(pageInfo);
+
+  const nextArrow = document.createElement('span');
+  nextArrow.classList.add('arrow', 'next');
+  nextArrow.innerHTML = '→';
+  nextArrow.addEventListener('click', () => showPage(currentPage + 1, requests, container, requestType));
+  if (currentPage === numPages) {
+    nextArrow.classList.add('disabled');
+  }
+  pagesDiv.appendChild(nextArrow);
+
+  container.appendChild(pagesDiv);
+}
+
+function showPage(pageNum, requests, container, requestType) {
+  const itemsPerPage = 10;
+  const startIndex = (pageNum - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentRequests = requests.slice(startIndex, endIndex);
+  const numPages = Math.ceil(requests.length / itemsPerPage);
+  if(pageNum>numPages){
+    return ;
+  }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  currentRequests.forEach((request) => {
+    const requestDiv = document.createElement('div');
+    requestDiv.classList.add(`${requestType}-requests`);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add(`${requestType}-info`);
+
+    const mainP = document.createElement('p');
+    mainP.classList.add(`${requestType}-main`);
+    mainP.textContent = `[${request.room_name}], ${request.day} ${request.busy_from}:00 - ${request.busy_to}:00`;
+
+    const descriptionP = document.createElement('p');
+    descriptionP.classList.add(`${requestType}-description`);
+    descriptionP.textContent = request.description;
+
+    infoDiv.appendChild(mainP);
+    infoDiv.appendChild(descriptionP);
+
+    if (requestType === 'pending') {
+      const nameP = document.createElement('p');
+      nameP.classList.add('pending-name');
+      nameP.textContent = request.event_name;
+
+      const renterP = document.createElement('p');
+      renterP.classList.add('pending-renter');
+      renterP.textContent = request.renter;
+
+      const numP = document.createElement('p');
+      numP.classList.add('pending-num');
+      numP.textContent = request.phone_number;
+
+      infoDiv.appendChild(nameP);
+      infoDiv.appendChild(renterP);
+      infoDiv.appendChild(numP);
+
+      const buttonsDiv = document.createElement('div');
+      buttonsDiv.classList.add('pending-buttons');
+
+      const acceptButton = document.createElement('button');
+      acceptButton.classList.add('accept');
+      acceptButton.textContent = 'Підтвердити';
+      acceptButton.addEventListener('click', () => updateRequestStatus(request, 1, requestDiv));
+
+      const declineButton = document.createElement('button');
+      declineButton.classList.add('decline');
+      declineButton.textContent = 'Відмовити';
+      declineButton.addEventListener('click', () => updateRequestStatus(request, 2, requestDiv));
+
+      buttonsDiv.appendChild(acceptButton);
+      buttonsDiv.appendChild(declineButton);
+
+      requestDiv.appendChild(infoDiv);
+      requestDiv.appendChild(buttonsDiv);
     } else {
-        loadingText.remove();
-
-        pendingRequests.forEach((request, index) => {
-            // if (index >= 10) return; // Show only the first 10 requests
-
-            const requestDiv = document.createElement('div');
-            requestDiv.classList.add('pending-requests');
-
-            const infoDiv = document.createElement('div');
-            infoDiv.classList.add('pending-info');
-
-            const mainP = document.createElement('p');
-            mainP.classList.add('pending-main');
-            mainP.textContent = `[${request.room_name}], ${request.day} ${request.busy_from}:00 - ${request.busy_to}:00`;
-
-            const nameP = document.createElement('p');
-            nameP.classList.add('pending-name');
-            nameP.textContent = request.event_name;
-
-            const descriptionP = document.createElement('p');
-            descriptionP.classList.add('pending-description');
-            descriptionP.textContent = request.description;
-
-            const renterP = document.createElement('p');
-            renterP.classList.add('pending-renter');
-            renterP.textContent = request.renter;
-
-            const numP = document.createElement('p');
-            numP.classList.add('pending-num');
-            numP.textContent = request.phone_number;
-
-            infoDiv.appendChild(mainP);
-            infoDiv.appendChild(nameP);
-            infoDiv.appendChild(descriptionP);
-            infoDiv.appendChild(renterP);
-            infoDiv.appendChild(numP);
-
-            const buttonsDiv = document.createElement('div');
-            buttonsDiv.classList.add('pending-buttons');
-
-            const acceptButton = document.createElement('button');
-            acceptButton.classList.add('accept');
-            acceptButton.textContent = 'Підтвердити';
-            acceptButton.addEventListener('click', () => updateRequestStatus(request, 1, requestDiv));
-
-            const declineButton = document.createElement('button');
-            declineButton.classList.add('decline');
-            declineButton.textContent = 'Відмовити';
-            declineButton.addEventListener('click', () => updateRequestStatus(request, 2, requestDiv));
-
-            buttonsDiv.appendChild(acceptButton);
-            buttonsDiv.appendChild(declineButton);
-
-            requestDiv.appendChild(infoDiv);
-            requestDiv.appendChild(buttonsDiv);
-
-            container.appendChild(requestDiv);
-            container.appendChild(document.createElement('hr'));
-        });
-
-        // if (pendingRequests.length > 10) {
-        //     const pagesDiv = document.createElement('div');
-        //     pagesDiv.classList.add('pages');
-        //     container.appendChild(pagesDiv);
-        //     // Add pagination logic here
-        // }
+      const statusDiv = document.createElement('div');
+      statusDiv.classList.add('prev-status');
+      if (request.status === 1) {
+        statusDiv.textContent = 'Підтверджено';
+        statusDiv.style.backgroundColor = "rgba(46, 171, 33, 200)";
+      } else {
+        statusDiv.textContent = 'Відмовлено';
+        statusDiv.style.backgroundColor = "rgba(213, 33, 47, 200)";
+      }
+      statusDiv.style.color = "#ffffff";
+      requestDiv.appendChild(infoDiv);
+      requestDiv.appendChild(statusDiv);
     }
-})
-.catch(error => console.error('Error fetching pending requests:', error));
+
+    container.appendChild(requestDiv);
+    container.appendChild(document.createElement('hr'));
+  });
+
+  const pagesDiv = container.querySelector('.pages');
+  if (pagesDiv) {
+    pagesDiv.remove();
+  }
+  createPaginationArrows(numPages, pageNum, container, requests, requestType);
+}
+
+fetch('/get_pending_requests')
+  .then(response => response.json())
+  .then(pendingRequests => {
+    if (pendingRequests.length === 0) {
+      loadingText.textContent = 'Наразі немає активних запитів';
+    } else {
+      loadingText.remove();
+
+      const itemsPerPage = 10; 
+      const numPages = Math.ceil(pendingRequests.length / itemsPerPage);
+
+      createPaginationArrows(numPages, 1, container, pendingRequests, 'pending');
+
+      showPage(1, pendingRequests, container, 'pending');
+    }
+  })
+  .catch(error => console.error('Error fetching pending requests:', error));
+
 const prevContainer = document.getElementById('prevRequestsContainer');
 const prevLoadingText = document.createElement('p');
 prevLoadingText.textContent = 'Завантаження даних...';
-prevLoadingText.style.fontSize="24px";
-prevLoadingText.style.marginLeft="10px";
+prevLoadingText.style.fontSize = "24px";
+prevLoadingText.style.marginLeft = "10px";
 prevContainer.appendChild(prevLoadingText);
+
 fetch('/get_past_requests')
-.then(response => response.json())
-.then(pastRequests => {
-    
+  .then(response => response.json())
+  .then(pastRequests => {
     if (pastRequests.length === 0) {
-        prevLoadingText.textContent = 'Наразі немає активних запитів';
+      prevLoadingText.textContent = 'Наразі немає активних запитів';
     } else {
-        pastRequests.forEach(request => {
-            prevLoadingText.remove();
-            const requestDiv = document.createElement('div');
-            requestDiv.classList.add('prev-requests');
+      prevLoadingText.remove();
 
-            const infoDiv = document.createElement('div');
-            infoDiv.classList.add('prev-info');
+      const itemsPerPage = 10; 
+      const numPages = Math.ceil(pastRequests.length / itemsPerPage);
 
-            const mainP = document.createElement('p');
-            mainP.classList.add('prev-main');
-            mainP.textContent = `[${request.room_name}], ${request.day} ${request.busy_from} - ${request.busy_to}`;
-
-            const descriptionP = document.createElement('p');
-            descriptionP.classList.add('prev-description');
-            descriptionP.textContent = request.description;
-
-            infoDiv.appendChild(mainP);
-            infoDiv.appendChild(descriptionP);
-
-            const statusDiv = document.createElement('div');
-            statusDiv.classList.add('prev-status');
-            if (request.status === 1) {
-                statusDiv.textContent = 'Підтверджено';
-                statusDiv.style.backgroundColor= "rgba(46, 171, 33, 200)";
-            } else {
-                statusDiv.textContent = 'Відмовлено';
-                statusDiv.style.backgroundColor= "rgba(213, 33, 47, 200)";
-            }
-            statusDiv.style.color="#ffffff";
-            requestDiv.appendChild(infoDiv);
-            requestDiv.appendChild(statusDiv);
-
-            prevContainer.appendChild(requestDiv);
-            prevContainer.appendChild(document.createElement('hr'));
-        });
+      createPaginationArrows(numPages, 1, prevContainer, pastRequests, 'prev');
+      showPage(1, pastRequests, prevContainer, 'prev');
     }
-})
-.catch(error => console.error('Error fetching past requests:', error));
+  })
+  .catch(error => console.error('Error fetching past requests:', error));
 
 function updateRequestStatus(request, newStatus, requestDiv) {
-    const requestData = {
-        room_name: request.room_name,
-        busy_from: request.busy_from,
-        busy_to: request.busy_to,
-        day: request.day,
-        renter: request.renter,
-        event_name: request.event_name,
-        description: request.description,
-        status: request.status,
-        new_status: newStatus,
-        available: []
-    };
+  const requestData = {
+    room_name: request.room_name,
+    busy_from: request.busy_from,
+    busy_to: request.busy_to,
+    day: request.day,
+    renter: request.renter,
+    event_name: request.event_name,
+    description: request.description,
+    status: request.status,
+    new_status: newStatus,
+    available: []
+  };
 
-    fetch('/update_request_status', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestData)
-    })
+  fetch('/update_request_status', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  })
     .then(response => {
-        if (response.ok) {
-            console.log('Request status updated successfully');
-            requestDiv.remove(); // Remove the request from the UI
-        } else {
-            console.error('Error updating request status');
-        }
+      if (response.ok) {
+        console.log('Request status updated successfully');
+        requestDiv.remove(); 
+      } else {
+        console.error('Error updating request status');
+      }
     })
     .catch(error => console.error('Error updating request status:', error));
 }
--- static/scripts/user_requests.js
+++ static/scripts/user_requests.js
@@ -1,53 +1,128 @@
+const requestsContainer = document.getElementById('requestsContainer');
+const loadingText = document.createElement('p');
+loadingText.textContent = 'Завантаження даних...';
+loadingText.style.fontSize = "24px";
+loadingText.style.marginLeft = "10px";
+loadingText.style.marginBottom = "40vw";
+requestsContainer.appendChild(loadingText);
+
+console.log("SSA");
+
+function createPaginationArrows(numPages, currentPage, container, requests) {
+  const pagesDiv = document.createElement('div');
+  pagesDiv.classList.add('pages');
+
+  const prevArrow = document.createElement('span');
+  prevArrow.classList.add('arrow', 'prev');
+  prevArrow.innerHTML = '←'
+  prevArrow.addEventListener('click', () => showPage(currentPage - 1, requests, container));
+  if (currentPage === 1) {
+    prevArrow.classList.add('disabled');
+  }
+  pagesDiv.appendChild(prevArrow);
+
+  const pageInfo = document.createElement('span');
+  pageInfo.classList.add('page-info');
+  pageInfo.textContent = ` ${currentPage} / ${numPages} `;
+  pagesDiv.appendChild(pageInfo);
+
+  const nextArrow = document.createElement('span');
+  nextArrow.classList.add('arrow', 'next');
+  nextArrow.innerHTML = '→';
+  nextArrow.addEventListener('click', () => showPage(currentPage + 1, requests, container));
+  if (currentPage === numPages) {
+    nextArrow.classList.add('disabled');
+  }
+  pagesDiv.appendChild(nextArrow);
+
+  container.appendChild(pagesDiv);
+}
+
+function showPage(pageNum, requests, container) {
+  const itemsPerPage = 10;
+  const startIndex = (pageNum - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentRequests = requests.slice(startIndex, endIndex);
+  const numPages = Math.ceil(requests.length / itemsPerPage);
+  if (pageNum>numPages || pageNum<1){
+    return ;
+  }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  if (requests.length === 0) {
+    const noRequestsText = document.createElement('p');
+    noRequestsText.textContent = 'На жаль, у Вас немає жодних запитів.';
+    noRequestsText.style.fontSize = "24px";
+    noRequestsText.style.marginLeft = "10px";
+    container.appendChild(noRequestsText);
+  } else {
+    currentRequests.forEach(request => {
+      requestsContainer.appendChild(document.createElement('hr'));
+
+      const requestDiv = document.createElement('div');
+      requestDiv.classList.add('request');
+
+      const requestInfoDiv = document.createElement('div');
+      requestInfoDiv.classList.add('request-info');
+
+      const mainInfoP = document.createElement('p');
+      mainInfoP.classList.add('request-main');
+      mainInfoP.textContent = `Аудиторія: ${request.room_name}; ${request.day}, ${request.busy_from}:00 - ${request.busy_to}:00`;
+
+      const descriptionP = document.createElement('p');
+      descriptionP.classList.add('request-description');
+      descriptionP.textContent = `${request.event_name}: ${request.description}`;
+
+      requestInfoDiv.appendChild(mainInfoP);
+      requestInfoDiv.appendChild(descriptionP);
+
+      const statusDiv = document.createElement('div');
+      statusDiv.classList.add("status");
+      statusDiv.textContent = "В очікуванні...";
+
+      if (request.status === 1) {
+        statusDiv.classList.add("status-confirm");
+        statusDiv.textContent = "Підтверджено";
+      }
+
+      if (request.status === 2) {
+        statusDiv.classList.add("status-decline");
+        statusDiv.textContent = "Відмовлено";
+      }
+
+      requestDiv.appendChild(requestInfoDiv);
+      requestDiv.appendChild(statusDiv);
+
+      requestsContainer.appendChild(requestDiv);
+    });
+  }
+
+  const pagesDiv = container.querySelector('.pages');
+  if (pagesDiv) {
+    pagesDiv.remove();
+  }
+  if (requests.length > 0) {
+    createPaginationArrows(numPages, pageNum, container, requests);
+  }
+}
+
 fetch('/requests/current_user')
   .then(response => response.json())
   .then(data => {
-    const requestsContainer = document.getElementById('requestsContainer');
+    if (data.length === 0) {
+      loadingText.textContent = "На жаль, у Вас немає жодних запитів.";
+      console.log("HI");
+    } else {
+      loadingText.remove();
 
-    // requestsContainer.innerHTML = '';
-    if(data.length == 0){
-      const text=document.getElementById("loadingText");
-      text.textContent="На жаль, у Вас немає жодних запитів."
-      console.log("HI")
-    }
-    else {
-      data.forEach(request => {
-        requestsContainer.appendChild(document.createElement('hr'));
-        const requestDiv = document.createElement('div');
-        requestDiv.classList.add('request');
+      const itemsPerPage = 10; 
+      const numPages = Math.ceil(data.length / itemsPerPage);
 
-        const requestInfoDiv = document.createElement('div');
-        requestInfoDiv.classList.add('request-info');
+      createPaginationArrows(numPages, 1, requestsContainer, data);
 
-        const mainInfoP = document.createElement('p');
-        mainInfoP.classList.add('request-main');
-        mainInfoP.textContent = `Аудиторія: ${request.room_name}; ${request.day}, ${request.busy_from}:00 - ${request.busy_to}:00`;
-
-        const descriptionP = document.createElement('p');
-        descriptionP.classList.add('request-description');
-        descriptionP.textContent = `${request.event_name}: ${request.description}`;
-
-        requestInfoDiv.appendChild(mainInfoP);
-        requestInfoDiv.appendChild(descriptionP);
-
-        const statusDiv = document.createElement('div');
-        statusDiv.classList.add("status")
-        statusDiv.textContent = "В очікуванні...";
-        if(request.status === 1){
-          statusDiv.classList.add("status-confirm")
-          statusDiv.textContent = "Підтверджено";
-        }
-        if(request.status === 2){
-          statusDiv.classList.add("status-decline")
-          statusDiv.textContent = "Відмовлено";
-        }
-        
-
-        requestDiv.appendChild(requestInfoDiv);
-        requestDiv.appendChild(statusDiv);
-
-        requestsContainer.appendChild(requestDiv);
-      });
+      showPage(1, data, requestsContainer);
     }
   })
   .catch(error => console.error('Error:', error));
-console.log("ХУЙ");
