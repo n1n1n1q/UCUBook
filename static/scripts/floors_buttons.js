d9a528a8132cb444b8d8dfe5c14211849c3732e2
annastasyshyn <anna.stasyshynn@gmail.com>
2024-03-12 02:21:38 +0200

    цш map displayed

--- static/scripts/floors_buttons.js
+++ static/scripts/floors_buttons.js
@@ -4,6 +4,13 @@ const buildingsData = [
     {"building": "Храм Софії", "floors": 2},
 ];
 
+const mapImages = ["ЦШ0", "ЦШ1", "ЦШ2", "ЦШ3", "ЦШ4"];
+
+function getFloorId(buildingName,floorNum) {
+    const tmp = buildingName.split(" ");
+    return tmp[0][0].toUpperCase()+tmp[1][0].toUpperCase()+floorNum;
+}
+
 function generateButtons(data) {
     const buildingsContainer = document.getElementById('buildingsContainer');
     const floorsContainer = document.getElementById('floorsContainer');
@@ -26,7 +33,7 @@ function generateButtons(data) {
         for (let i = 0; i < buildingData.floors; i++) {
             const floorButton = document.createElement('button');
             floorButton.classList.add('floor-button');
-            floorButton.id = buildingData.building+'-floor-'+i;
+            floorButton.id = getFloorId(buildingData.building,i);
             floorButton.addEventListener('click', () => {
                 activeFloor(floorButton.id);
             });
@@ -36,7 +43,7 @@ function generateButtons(data) {
     });
     const buildingNames = buildingsData.map(data => data.building);
     showFloors(buildingNames[0]);
-    activeFloor(buildingNames[0]+'-floor-0');
+    activeFloor(getFloorId(buildingNames[0],0));
 }
 
 function showFloors(buildingName) {
@@ -48,12 +55,14 @@ function showFloors(buildingName) {
     allBuildings.forEach(building => {
         if (building.id !== buildingName+'-button') {
             building.classList.remove('current');
-        } else {building.classList.add('current');}
+        } else {
+            building.classList.add('current');
+        }
     });
     const floorsList = document.getElementById(buildingName);
     if (floorsList) {floorsList.classList.add('active');
     }
-    activeFloor(buildingName+"-floor-0");
+    activeFloor(buildingName,0);
 }
 
 function activeFloor(floorId) {
@@ -63,12 +72,32 @@ function activeFloor(floorId) {
             floor.classList.remove('current');
         } else {
             floor.classList.add('current');
+            showMap(floorId);
         }
     });
 }
 
-function generateMap (floor) {
-    // !!!
+function showMap(floorId) {
+    const mapContainer = document.getElementsByClassName("map-container")[0];
+    mapContainer.innerHTML = "";
+    if (mapImages.indexOf(floorId)===-1) {
+        const noImage = document.createElement("p");
+        noImage.textContent = "Немає карти";
+        mapContainer.appendChild(noImage);
+    } else {
+        const mapImage = document.createElement("img");
+        mapImage.classList.add("map-image");
+        floorFolder = floorId.slice(0,2);
+        fileName = `${floorId[2]}pov.jpg`;
+        mapImage.src = `static/assets/${floorFolder}/${fileName}`;
+        mapImage.style.width = "relative";
+        mapImage.style.padding = "4px";
+        mapImage.style.marginTop = "15px";
+        mapImage.style.marginLeft = "20px";
+        mapImage.style.height = "50rem";
+        mapContainer.appendChild(mapImage);
+    }
 }
 
-generateButtons(buildingsData)
+// showMap("ЦШ0");
+generateButtons(buildingsData);
