4a9e9415002b2071c00d42031ef42fa6fdc95afe
Oleh Basystyi <basystyo@gmail.com>
2024-03-26 00:57:43 +0200

    port change + styles fix

--- src/main.py
+++ src/main.py
@@ -185,4 +185,4 @@ app.include_router(user.user_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
